54a1037d377baada4459b9261c59f43253d1860e
kmwyatt <arc2437@naver.com>
2022-02-06 02:00:59 +0900

    Modify authority

--- index.js
+++ index.js
@@ -70,7 +70,6 @@ app.post("/api/users/login", (req, res) => {
 app.get("/api/users/auth", auth, (req, res) => {
   res.status(200).json({
     _id: req.user._id,
-    isAdmin: req.user.role === 2 ? true : false,
     isAuth: true,
     id: req.user.id,
     name: req.user.name,
